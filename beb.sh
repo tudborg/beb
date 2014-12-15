5985575d1fb884c9ab52b5b95af30be4891ea1ed
Henrik Tudborg <henrik@tudb.org>
2014-12-15 13:53:25 +0100

    Resolve script dir even behind symlinks

--- beb.sh
+++ beb.sh
@@ -3,7 +3,15 @@
 unset CDPATH
 
 # define variables available in all modules
-SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$SCRIPT_DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 MODULES_DIR="$SCRIPT_DIR/modules"
 SCRIPT_MAIN="${BASH_SOURCE[0]}"
 MODULE_FILE_POSTFIX=".module.sh"
