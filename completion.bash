34010861fc7691178ad91c64a3fa0117cb8c0629
Henrik Tudborg <henrik@tudb.org>
2015-01-06 14:47:34 +0100

    Start of a completion script.

---
+++ completion.bash
@@ -0,0 +1,33 @@
+_beb() 
+{
+    local cur prev opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+
+    #
+    #  Complete the arguments to some of the basic commands.
+    #
+    case "${prev}" in
+        beb)
+            opts="build environment release upload version"
+            COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+            return 0
+            ;;
+        version)
+            opts="create list"
+            COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+            return 0
+            ;;
+        environment)
+            opts="info"
+            COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+            return 0
+            ;;
+        *)
+            ;;
+    esac
+   return 0
+}
+complete -F _beb beb
